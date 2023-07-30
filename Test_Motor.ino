8bbfa023b97ff98be8c4b64378c95d1ad85a2344
yanuiskan <140927334+yanuiskan@users.noreply.github.com>
2023-07-30 09:24:45 +0800

    Add files via upload

---
+++ Test_Motor.ino
@@ -0,0 +1,27 @@
+ 
+ int rpm = 186;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(5,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int noChar = Serial.available(); 
+  if(noChar) {
+    int rpmMotor = Serial.read();
+    if(rpmMotor==97)  rpm += 1;
+    if(rpmMotor==122) rpm -= 1;
+    if(rpmMotor==32)  rpm = 186; 
+    rpm = constrain(rpm,0,255);
+     analogWrite(5,rpm);
+     Serial.print("PWM");
+     Serial.print('\t');
+     Serial.print(rpm);
+     Serial.print('\t');
+     Serial.println(rpmMotor);
+  } 
+delay(5);
+}
